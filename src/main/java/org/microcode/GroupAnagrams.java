6deeb4953516c751caaa77ece2038cc5acc039c5
Dastan <asymbaevdastan07@gmail.com>
2024-05-28 17:25:08 -0400

    solved productOfArray

--- src/main/java/org/microcode/GroupAnagrams.java
+++ src/main/java/org/microcode/GroupAnagrams.java
@@ -3,22 +3,4 @@ package org.microcode;
 import java.util.*;
 
 public class GroupAnagrams {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return new ArrayList<>();
-        }
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (String s : strs) {
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            String sortedKey = new String(chars);
-
-            if (!map.containsKey(sortedKey)) {
-                map.put(sortedKey, new ArrayList<>());
-            }
-            map.get(sortedKey).add(s);
-        }
-        return new ArrayList<>(map.values());
-    }
-}
+    public List<List<String>>
