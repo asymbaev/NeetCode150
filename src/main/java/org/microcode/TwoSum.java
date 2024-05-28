f8f538e5c09a7437f5696c4e9328339e020496c1
Dastan <asymbaevdastan07@gmail.com>
2024-05-27 20:22:06 -0400

    solved a TwoSum

---
+++ src/main/java/org/microcode/TwoSum.java
@@ -0,0 +1,21 @@
+package org.microcode;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length ; i++) {
+            int n = nums[i];
+            int diff = target - n;
+            if (prevMap.containsKey(diff)) {
+                return new int[]{prevMap.get(diff), i};
+            }
+            prevMap.put(n, i);
+
+        }
+        return new int[0];
+    }
+}
