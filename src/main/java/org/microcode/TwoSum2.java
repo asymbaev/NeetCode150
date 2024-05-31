0d80aae0c65b8fa0cfbf3c18c6810799959bdb10
Dastan <asymbaevdastan07@gmail.com>
2024-05-30 22:45:09 -0400

    cmplt

---
+++ src/main/java/org/microcode/TwoSum2.java
@@ -0,0 +1,21 @@
+package org.microcode;
+
+public class TwoSum2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length - 1;
+
+        while (l < r) {
+            int curSum = numbers[l] + numbers[r];
+
+            if (curSum > target) {
+                r -= 1;
+            } else if (curSum < target) {
+                l += 1;
+            } else {
+                return new int[]{l + 1, r + 1};
+            }
+        }
+        return new int[0];
+    }
+}
